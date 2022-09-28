ebb279021e6554bc6cea69f799398c3c757ec0f9
Manish Jaiswal <jaiswal.manish49@gmail.com>
2022-09-28 10:09:11 +0530

    Backend Project setup completed

---
+++ SuggestionAppService/SuggestionApp.Utilities/Mapper/MapperBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuggestionApp.Utilities.Mapper
+{
+    public abstract class MapperBase<TModel, TDto> where TModel : class where TDto: new()
+    {
+        public TDto MapToDto(TModel model)
+        {
+            try
+            {
+                return this.MapToDtoInternal(model);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<TDto> MapToDto(IEnumerable<TModel> modelList)
+        {
+            foreach(var model in modelList)
+            {
+                yield return this.MapToDto(model);
+            }
+        }
+
+        public TModel MapToModel(TDto dto)
+        {
+            try
+            {
+                return this.MapToModelInternal(dto);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<TModel> MapToModel(IEnumerable<TDto> dtolList)
+        {
+            foreach (var dto in dtolList)
+            {
+                yield return this.MapToModel(dto);
+            }
+        }
+
+        public virtual TDto MapToDtoInternal(TModel model)
+        {
+            throw new Exception("method not implemented");
+        }
+
+        public virtual TModel MapToModelInternal(TDto dto)
+        {
+            throw new Exception("method not implemented");
+        }
+    }
+}
---
+++ SuggestionAppService/SuggestionApp.WebApi/Infrastructure/RegisterServices.cs
@@ -0,0 +1,16 @@
+﻿namespace SuggestionApp.WebApi.Infrastructure
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddMemoryCache();
+        }
+    }
+}
